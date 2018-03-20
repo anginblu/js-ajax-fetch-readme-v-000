022bd25b579c683890c79fde05a4c74532717ec4
Jacqueline <anginblu@gmail.com>
2018-03-19 23:43:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'YOUR_TOKEN_HERE'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${53b867317cbba365a67090e6e83598292e325726}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
